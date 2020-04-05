feae7e7a5a636e46eedae1d5002e80e18bea2d4b
shamisonn <unknown0incident@gmail.com>
2020-04-06 00:31:19 +0900

    modify; no mutable code

--- main.py
+++ main.py
@@ -26,17 +26,17 @@ def preformat(img):
 
 # return gray(crop(img, 184, 235, 27, 60))
 
-def write(img, out: str):
-    cv2.imwrite(out, img)
+def convert_cv2_pil(img):
+    return Image.fromarray((cv2.cvtColor(img, cv2.COLOR_BGR2RGB)))
 
 
 tools = pyocr.get_available_tools()
 
 
 # 画像から文字を生成
-def image2text(path) -> str:
+def image2text(img) -> str:
     return tools[0].image_to_string(
-        Image.open(path),
+        img,
         lang='eng',
         builder=pyocr.builders.TextBuilder(tesseract_layout=6))
 
@@ -48,8 +48,7 @@ def clean(text: str) -> str:
 
 def main() -> ():
     out_name = 'out.png'
-    write(preformat(get_img('kabu.jpeg')), out_name)
-    print(clean(image2text(out_name)))
+    print(clean(image2text(convert_cv2_pil(preformat(get_img('kabu.jpeg'))))))
 
 
 if __name__ == '__main__':
